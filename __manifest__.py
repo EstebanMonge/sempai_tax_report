bfe1c144951179d90a2770cb2217aba84390371a
Esteban Monge <estebanmonge@riseup.net>
2026-08-19 13:19:24 -0600

    initial release

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Sempai Tax Report',
-    'version': '15.0.1.0.0',
+    'version': '17.0.1.0.0',
     'category': 'Reporting',
     'summary': 'Sempai tax PDF Reports',
     'author': 'Sempai Space',
